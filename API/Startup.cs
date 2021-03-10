c9b2cc94797f6ed3d81c49e6cf3f73913ac6c1e7
Wagner Martins <wagnermartins@wagnermac.local>
2021-03-10 20:14:14 -0300

    see tomorrow

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,6 +48,9 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
