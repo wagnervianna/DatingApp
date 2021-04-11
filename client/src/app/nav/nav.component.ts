a62425414d5efa117a013bf2511c513f48c5f3ad
Wagner Martins <wagnermartins@wagnermac.local>
2021-04-10 22:30:14 -0300

    turning public accountService

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AccountService } from '../services/account.service';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,34 +10,21 @@ import { AccountService } from '../services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
   // tslint:disable-next-line:typedef
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     });
   }
-
+  // tslint:disable-next-line:typedef
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
-  }
-
-  // tslint:disable-next-line:typedef
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe (user => {
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    }) 
   }
 }
