001860cd54c99bbdf641909bb63c67daeb7fc0c6
Wagner Martins <wagnermartins@wagnermac.local>
2021-08-02 20:03:32 -0300

    test token

--- API/Program.cs
+++ API/Program.cs
@@ -29,7 +29,7 @@ namespace API
             catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error ocurred during migration");
+                logger.LogError(ex, "An error ocurred during migrations");
             }
 
             await host.RunAsync();
