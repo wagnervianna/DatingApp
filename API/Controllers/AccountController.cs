c781511330f950e2c8d6ab31061ebd2f7b0f77c3
Wagner Martins <wagnermartins@wagnermac.local>
2022-07-11 21:47:49 -0300

    bugfix/teste

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -61,7 +61,7 @@ namespace API.Controllers
             for(int i=0; i < computedHash.Length; i++)
             {
                 if (computedHash[i] != user.PasswordHash[i]) {
-                    return Unauthorized("Invalid password");
+                    return Unauthorized("Invalid Password");
                 }
             }
 
